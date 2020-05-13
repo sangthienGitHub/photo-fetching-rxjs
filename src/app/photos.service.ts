ac0f806eb75437efdbc98a708d28bbcb552a2894
sangTran <sangthien2010@gmail.com>
2020-05-13 16:19:58 -0500

    using interface for response data type

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -9,10 +15,13 @@ export class PhotosService {
   constructor(private http: HttpClient) {}
 
   getPhoto() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
-      headers: {
-        Authorization: `Client-ID ${environment.unsplashApiKey}`,
-      },
-    });
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization: `Client-ID ${environment.unsplashApiKey}`,
+        },
+      }
+    );
   }
 }

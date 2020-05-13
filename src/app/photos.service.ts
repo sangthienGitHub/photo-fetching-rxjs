cb72a483a76f4ea27cd0d9bbe90025ebfc3836cb
sangTran <sangthien2010@gmail.com>
2020-05-13 16:01:05 -0500

    making a request call to unsplash api

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotosService {
   constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos.random', {
+      headers: {
+        Authorization: `Client-ID ${environment.unsplashApiKey}`
+      }
+    }
 }
